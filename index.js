04f9fd71b4ed52506a421b123ae3d113de0074df
Abhinav Kushwaha <abhinav@ipsator.com>
2020-05-10 23:56:29 +0530

    v1.0.9 crypto fixes

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 
+const BrowserUtils = require("./lib/browser");
+const CryptoUtils = require("./lib/crypto");
 const TimeUtils = require("./lib/time");
 
-exports = module.exports = { TimeUtils };
+exports.BrowserUtils = BrowserUtils
+exports.CryptoUtils = CryptoUtils
+exports.TimeUtils = TimeUtils
